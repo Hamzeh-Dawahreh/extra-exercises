7224c8f3f8515e906bd9513159a928681d5c832b
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-03-17 18:06:29 +0300

    added new file

---
+++ app.js
@@ -0,0 +1,72 @@
+// This is Q1 in extra exercises
+console.log(-9 * 3);
+console.log("value is " + 50);
+console.log(17 % 5);
+console.log(5 % 17);
+console.log(5 / 10);
+console.log(4 == 4);
+console.log(4 != 5);
+console.log(7 <= 8);
+x = 5.2;
+Math.ceil(x) - Math.floor(x);
+
+// This is Q2 in extra exercises
+
+let number = prompt("Enter a number");
+
+alert(number);
+
+// This is Q3 in extra exercises
+
+let first = prompt("Enter The first number");
+let second = prompt("Enter The second number");
+
+if (first > second) {
+  alert(second);
+  alert(first);
+} else {
+  alert(first);
+  alert(second);
+}
+// This is Q4 in extra exercises
+
+let first1 = prompt("Enter The first number");
+let second1 = prompt("Enter The second number");
+
+if (first1 > second1) {
+  alert(first1);
+} else {
+  alert(second1);
+}
+// This is Q5 in extra exercises
+
+let first2 = prompt("Enter The first number");
+let second2 = prompt("Enter The second number");
+
+alert(first2 + second2);
+//it read them as strings
+
+// This is Q6 in extra exercises
+let num = prompt("Enter a Number");
+
+if (num == 1) {
+  console.log("ONE");
+} else if (num == 2) {
+  console.log("TWO");
+} else if (num == 3) {
+  console.log("THREE");
+} else if (num == 4) {
+  console.log("FOUR");
+} else if (num == 5) {
+  console.log("FIVE");
+} else if (num == 6) {
+  console.log("SIX");
+} else if (num == 7) {
+  console.log("SEVEN");
+} else if (num == 8) {
+  console.log("EIGHT");
+} else if (num == 9) {
+  console.log("NINE");
+} else {
+  console.log("PLEASE TRY AGAIN");
+}
