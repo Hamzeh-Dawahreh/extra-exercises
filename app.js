57c94a890f82989b637b02aac1b1345cc76c18af
Dawahreh <hamza.mohmmad99@yahoo.com>
2023-03-20 01:08:55 +0300

    New exercises

--- app.js
+++ app.js
@@ -1,72 +1,117 @@
-// This is Q1 in extra exercises
-console.log(-9 * 3);
-console.log("value is " + 50);
-console.log(17 % 5);
-console.log(5 % 17);
-console.log(5 / 10);
-console.log(4 == 4);
-console.log(4 != 5);
-console.log(7 <= 8);
-x = 5.2;
-Math.ceil(x) - Math.floor(x);
+// // This is Q1 in extra exercises
+// console.log(-9 * 3);
+// console.log("value is " + 50);
+// console.log(17 % 5);
+// console.log(5 % 17);
+// console.log(5 / 10);
+// console.log(4 == 4);
+// console.log(4 != 5);
+// console.log(7 <= 8);
+// x = 5.2;
+// Math.ceil(x) - Math.floor(x);
 
-// This is Q2 in extra exercises
+// // This is Q2 in extra exercises
 
-let number = prompt("Enter a number");
+// let number = prompt("Enter a number");
 
-alert(number);
+// alert(number);
 
-// This is Q3 in extra exercises
+// // This is Q3 in extra exercises
 
-let first = prompt("Enter The first number");
-let second = prompt("Enter The second number");
+// let first = prompt("Enter The first number");
+// let second = prompt("Enter The second number");
 
-if (first > second) {
-  alert(second);
-  alert(first);
-} else {
-  alert(first);
-  alert(second);
+// if (first > second) {
+//   alert(second);
+//   alert(first);
+// } else {
+//   alert(first);
+//   alert(second);
+// }
+// // This is Q4 in extra exercises
+
+// let first1 = prompt("Enter The first number");
+// let second1 = prompt("Enter The second number");
+
+// if (first1 > second1) {
+//   alert(first1);
+// } else {
+//   alert(second1);
+// }
+// // This is Q5 in extra exercises
+
+// let first2 = prompt("Enter The first number");
+// let second2 = prompt("Enter The second number");
+
+// alert(first2 + second2);
+// //it read them as strings
+
+// // This is Q6 in extra exercises
+// let num = prompt("Enter a Number");
+
+// if (num == 1) {
+//   console.log("ONE");
+// } else if (num == 2) {
+//   console.log("TWO");
+// } else if (num == 3) {
+//   console.log("THREE");
+// } else if (num == 4) {
+//   console.log("FOUR");
+// } else if (num == 5) {
+//   console.log("FIVE");
+// } else if (num == 6) {
+//   console.log("SIX");
+// } else if (num == 7) {
+//   console.log("SEVEN");
+// } else if (num == 8) {
+//   console.log("EIGHT");
+// } else if (num == 9) {
+//   console.log("NINE");
+// } else {
+//   console.log("PLEASE TRY AGAIN");
+// }
+
+
+// This is Q7 in extra exercises
+
+// for ( let i=0; i<=5; i++ ){
+//   alert(i);
+// }
+
+
+// This is Q8 in extra exercises
+// let space="";
+// for (let i = 0; i <= 5; i++) {
+//   space += i;
+// }
+// alert(space);
+
+// This is Q9 in extra exercises
+// for (let i = 0; i <= 20; i++) {
+// if (i % 3 === 0){
+// alert(i);
+
+// }
+// }
+
+
+// This is Q10 in extra exercises
+// do {
+// let number = prompt("Enter a number between 0-100")
+
+
+// if ( number >= 0 && number <=100 ){
+//   alert("Good boy")
+//   break;
+// }else  {
+//   alert ("areuseriousrightnowbro")
+// }
+// }while (true);
+
+// This is Q10 in extra exercises (another way to write the code)
+let number = prompt("Enter a number between 0-100")
+while (number < 0 || number > 100){
+  alert("areuseriousrightnowbro")
+  number = prompt("Enter a number between 0-100")
 }
-// This is Q4 in extra exercises
-
-let first1 = prompt("Enter The first number");
-let second1 = prompt("Enter The second number");
-
-if (first1 > second1) {
-  alert(first1);
-} else {
-  alert(second1);
-}
-// This is Q5 in extra exercises
-
-let first2 = prompt("Enter The first number");
-let second2 = prompt("Enter The second number");
-
-alert(first2 + second2);
-//it read them as strings
-
-// This is Q6 in extra exercises
-let num = prompt("Enter a Number");
-
-if (num == 1) {
-  console.log("ONE");
-} else if (num == 2) {
-  console.log("TWO");
-} else if (num == 3) {
-  console.log("THREE");
-} else if (num == 4) {
-  console.log("FOUR");
-} else if (num == 5) {
-  console.log("FIVE");
-} else if (num == 6) {
-  console.log("SIX");
-} else if (num == 7) {
-  console.log("SEVEN");
-} else if (num == 8) {
-  console.log("EIGHT");
-} else if (num == 9) {
-  console.log("NINE");
-} else {
-  console.log("PLEASE TRY AGAIN");
-}
+alert("Good boy")
